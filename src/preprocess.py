88b0d1ae195f9277cdfa5b9595c5e1ab99dde5dc
minsoo9506 <ghktjd15gh@gamil.com>
2021-11-20 16:03:41 +0900

    normalize_data 함수 추가

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,14 +3,29 @@ import numpy as np
 
 PATH = "../UCR_Anomaly_FullData/"
 
-# train에 기반하여 mean, std로 normalize하고
-def normalize(data_name: str):
+# train에 기반하여 mean, std로 normalize
+def normalize_data(data_name: str):
+    """train에 기반하여 mean, std로 normalize
+
+    Parameters
+    ----------
+    data_name : str
+
+    Returns
+    -------
+    normalize_data: pd.DataFrame
+    """
+
     data_path = PATH + data_name
     data_path_split = data_path.split("_")
     train_end_idx = data_path_split[4]
-    anomaly_start_idx, anomaly_end_idx = data_path_split[5], data_path_split[6]
 
     data = pd.read_csv(data_path)
+    train_data = data[:train_end_idx]
+    train_mean = np.mean(train_data)
+    train_std = np.std(train_data)
+    normalize_data = (data - train_mean) / train_std
+    return normalize_data
 
 
 # 데이터이름에 따라서 train, test로 나누기
