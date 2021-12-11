620f964d8e15c0fcfd5f6159f889933d837b3e13
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-11 12:28:10 +0900

    test

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
-PATH = "../UCR_Anomaly_FullData/"
-data_name = "001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620"
+PATH = "./UCR_Anomaly_FullData/"
+data_name = "001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620.txt"
 
 data_path = PATH + data_name
 
 import pandas as pd
-import numpy
+import numpy as np
+
+df = pd.read_csv(data_path, header=None)
+
+np_df = np.loadtxt(data_path)
+print(np_df[:4])
+
+tmp = np.zeros(5)
+tmp[1:2]=2
+print(tmp)
+
+
