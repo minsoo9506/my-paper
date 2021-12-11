40beb800b2a03186e586206e8b1fba0049c4cbb6
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-11 12:27:52 +0900

    BaseTrainer 코드 수정

--- src/trainer.py
+++ src/trainer.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 
-class Trainer:
+class BaseTrainer:
     def __init__(self, model, optimizer, crit):
         super().__init__()
         self.model = model
@@ -13,14 +13,11 @@ class Trainer:
     def _train(self, train_loader, device):
         self.model.train()
         total_loss = 0
-        for rnn_enc_x, cnn_enc_x, dec_x, target in train_loader:
+        for input_x, _ in train_loader:
             if device != "cpu":
-                rnn_enc_x = rnn_enc_x.to(device)
-                cnn_enc_x = cnn_enc_x.to(device)
-                dec_x = dec_x.to(device)
-                target = target.to(device)
-            y_hat = self.model(rnn_enc_x, cnn_enc_x, dec_x)
-            loss = self.crit(y_hat, target)
+                input_x = input_x.to(device)
+            y_hat = self.model(input_x)
+            loss = self.crit(y_hat, input_x)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
@@ -32,34 +29,36 @@ class Trainer:
         self.model.eval()
         with torch.no_grad():
             total_loss = 0
-            for rnn_enc_x, cnn_enc_x, dec_x, target in val_loader:
+            for input_x, _ in val_loader:
                 if device != "cpu":
-                    rnn_enc_x = rnn_enc_x.to(device)
-                    cnn_enc_x = cnn_enc_x.to(device)
-                    dec_x = dec_x.to(device)
-                    target = target.to(device)
-                y_hat = self.model(rnn_enc_x, cnn_enc_x, dec_x)
-                loss = self.crit(y_hat, target)
+                    input_x = input_x.to(device)
+                y_hat = self.model(input_x)
+                loss = self.crit(y_hat, input_x)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                # prevent memory leak
                 total_loss += float(loss)
             return total_loss / len(val_loader)
 
-    def train(self, train_loader, val_loader, config):
+    def train(self, train_loader, val_loader, config, use_wandb):
         lowest_loss = np.inf
         best_model = None
         early_stop_round = 0
 
-        import wandb
-
-        wandb.login()
-        wandb.init(project=config.project, config=config)
-        wandb.watch(self.model, self.crit, log="gradients", log_freq=100)
+        if use_wandb:
+            import wandb
+            wandb.login()
+            wandb.init(project=config.project, config=config)
+            wandb.watch(self.model, self.crit, log="gradients", log_freq=100)
 
         for epoch_index in range(config.n_epochs):
             train_loss = self._train(train_loader, config.device)
             valid_loss = self._validate(val_loader, config.device)
 
-            wandb.log({"train_loss": train_loss})
-            wandb.log({"valid_loss": valid_loss})
+            if use_wandb:
+                wandb.log({"train_loss": train_loss})
+                wandb.log({"valid_loss": valid_loss})
 
             if valid_loss < lowest_loss:
                 lowest_loss = valid_loss
