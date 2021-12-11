5c3e96b09705458c42f0a3300793799eb569c510
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-11 12:27:25 +0900

    WindowBasedDataset 만들기

--- src/dataload/window_based.py
+++ src/dataload/window_based.py
@@ -1,40 +1,37 @@
 import numpy as np
-import torch
 from torch.utils.data import Dataset
 
 
 class WindowBasedDataset(Dataset):
-    """make dataset with window based approach
-
-    Parameters
-    ----------
-    Dataset : [type]
-        [description]
-    """
 
     def __init__(
         self,
-        data: np.ndarray,
-        window: int,
-        seq_len: int,
-        feature_dim: int = 1,
-    ) -> None:
-        """[summary]
+        x: np.ndarray,
+        y: np.ndarray,
+        window: int
+    ):
+        """make window based data
 
         Parameters
         ----------
-        data : np.ndarray
-            [description]
-        window : int
-            [description]
-        seq_len : int
-            [description]
-        feature_dim : int, optional
-            [description], by default 1
+        x : np.ndarray
+            window 기반으로 만들기 이전 input data
+        y : np.ndarray
+            window 기반으로 만들기 이전 output data
         """
-        super().__init__()
 
-        # data preprocessing
-        _mean = np.mean(data)
-        _std = np.std(data)
-        data = (data - _mean) / _std
+        super().__init__()
+        
+        data_len = len(x) - window + 1
+        self.x = np.zeros((data_len, window))
+        self.y = np.zeros((data_len, window))
+        
+        for idx in data_len:
+            self.x[idx, :] = x[idx : idx + window]
+            self.y[idx, :] = y[idx : idx + window]
+        
+    def __len__(self):
+        return self.x.shape[0]
+    
+    def __getitem__(self, idx: int):
+        return self.x[idx, :], self.y[idx, :]
