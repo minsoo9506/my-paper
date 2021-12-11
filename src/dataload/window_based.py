fe53817842929851ac675596c7c574d5876ea559
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-12 00:00:46 +0900

    torch.tensor로 만드는 코드 추가

--- src/dataload/window_based.py
+++ src/dataload/window_based.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
@@ -18,6 +19,8 @@ class WindowBasedDataset(Dataset):
             window 기반으로 만들기 이전 input data
         y : np.ndarray
             window 기반으로 만들기 이전 output data
+        window : int
+            window size
         """
 
         super().__init__()
@@ -26,9 +29,12 @@ class WindowBasedDataset(Dataset):
         self.x = np.zeros((data_len, window))
         self.y = np.zeros((data_len, window))
         
-        for idx in data_len:
+        for idx in range(data_len):
             self.x[idx, :] = x[idx : idx + window]
             self.y[idx, :] = y[idx : idx + window]
+            
+        self.x = torch.tensor(self.x, dtype=torch.float32)
+        self.y = torch.tensor(self.y, dtype=torch.float32)
         
     def __len__(self):
         return self.x.shape[0]
