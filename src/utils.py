8a4f76a0d2f3b2764386496fb4762345822114d6
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-12 15:15:09 +0900

    f1_score 추가

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import recall_score, precision_score, accuracy_score, roc_auc_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score, roc_auc_score, f1_score
 import numpy as np
 
 
@@ -17,4 +17,5 @@ def get_score(y_true: np.ndarray, y_pred: np.ndarray, anomaly_score: np.ndarray)
     print(f"accuracy_score = {accuracy_score(y_true, y_pred):.3f}")
     print(f"precision_score = {precision_score(y_true, y_pred):.3f}")
     print(f"recall_score = {recall_score(y_true, y_pred):.3f}")
+    print(f"f1_score = {f1_score(y_true, y_pred):.3f}")
     print(f"roc_auc_score = {roc_auc_score(y_true, anomaly_score):.3f}")
