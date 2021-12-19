ab4d992831d14f261755f64fad76fbd35842236b
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-19 11:20:29 +0900

    WeightedWindowBasedDataset 추가

--- src/dataload/window_based.py
+++ src/dataload/window_based.py
@@ -47,3 +47,59 @@ class WindowBasedDataset(Dataset):
         self.y[idx, :] : torch.tensor
         """
         return self.x[idx, :], self.y[idx, :]
+
+class WeightedWindowBasedDataset(Dataset):
+    def __init__(self, x: np.ndarray, y: np.ndarray, window: int, weight: np.ndarray):
+        """make window based data, sampling with replacement
+
+        Parameters
+        ----------
+        x : np.ndarray
+            window 기반으로 만들기 이전 input data
+        y : np.ndarray
+            window 기반으로 만들기 이전 output data
+        window : int
+            window size
+        weight : np.ndarray
+            sampling시에 사용할 weight
+        """
+
+        super().__init__()
+
+        data_len = len(x) - window + 1
+        self.x = np.zeros((data_len, window))
+        self.y = np.zeros((data_len, window))
+
+        for idx in range(data_len):
+            self.x[idx, :] = x[idx : idx + window]
+            self.y[idx, :] = y[idx : idx + window]
+
+        # sample with replacement
+        idx = np.arange(len(self.x))
+        sampled_idx = np.random.choice(idx, size=len(idx), replace=True, p=weight)
+        sampled_idx = np.sort(sampled_idx)
+        
+        ####################################
+        # idx를 통해서 indexing하는 코드 추가
+        ####################################
+        
+        self.x = torch.tensor(self.x, dtype=torch.float32)
+        self.y = torch.tensor(self.y, dtype=torch.float32)
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx: int):
+        """[summary]
+
+        Parameters
+        ----------
+        idx : int
+            [description]
+
+        Returns
+        -------
+        self.x[idx, :] : torch.tensor
+        self.y[idx, :] : torch.tensor
+        """
+        return self.x[idx, :], self.y[idx, :]
