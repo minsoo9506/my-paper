a02e231dd7bffb064f2187e1683ed7a4501d287f
minsoo9506 <ghktjd15gh@gamil.com>
2021-12-19 11:19:25 +0900

    test

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
-PATH = "./UCR_Anomaly_FullData/"
-data_name = "001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620.txt"
+# PATH = "./UCR_Anomaly_FullData/"
+# data_name = "001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620.txt"
 
-data_path = PATH + data_name
+# data_path = PATH + data_name
 
-import pandas as pd
+# import pandas as pd
+# import numpy as np
+
+# df = pd.read_csv(data_path, header=None)
+
+# np_df = np.loadtxt(data_path)
+# print(np_df[:4])
+
+# tmp = np.zeros(5)
+# tmp[1:2]=2
+# print(tmp)
+
+import random
 import numpy as np
-
-df = pd.read_csv(data_path, header=None)
-
-np_df = np.loadtxt(data_path)
-print(np_df[:4])
-
-tmp = np.zeros(5)
-tmp[1:2]=2
-print(tmp)
-
-
+p = np.array([0.1,0.1,0.2,0.3,0.3])
+x = np.zeros((5,5))
+idx = np.arange(len(x))
+result = np.sort(np.random.choice(idx, size=len(idx) ,replace=True, p=p))
+print(result)
