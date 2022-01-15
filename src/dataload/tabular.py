412a79bb33b9166cb90381d1b871242e2643123f
minsoo9506 <ghktjd15gh@gamil.com>
2022-01-15 23:37:48 +0900

    WeightedtabularDataset 함수 작성

--- src/dataload/tabular.py
+++ src/dataload/tabular.py
@@ -14,16 +14,26 @@ class tabularDataset(Dataset):
         return self.x.shape[0]
 
     def __getitem__(self, idx: int):
-        """[summary]
+        return self.x[idx, :], self.y[idx, :]
 
-        Parameters
-        ----------
-        idx : int
-            [description]
 
-        Returns
-        -------
-        self.x[idx, :] : torch.tensor
-        self.y[idx, :] : torch.tensor
-        """
+class WeightedtabularDataset(Dataset):
+    def __init__(self, x: np.ndarray, y: np.ndarray, weight: np.ndarray):
+        super().__init__()
+
+        self.x = torch.tensor(x, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.float32)
+
+        # sample with replacement
+        idx = np.arange(len(x))
+        sampled_idx = np.random.choice(idx, size=len(idx), replace=True, p=weight)
+        sampled_idx = np.sort(sampled_idx)
+
+        self.x = self.x[sampled_idx]
+        self.y = self.y[sampled_idx]
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx: int):
         return self.x[idx, :], self.y[idx, :]
